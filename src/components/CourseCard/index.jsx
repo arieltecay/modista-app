29e3d79a9c56957d0c77962c3931df01fb473579
arieltecay <arieltecay@gmail.com>
2025-08-01 23:29:30 -0300

    cover images

--- src/components/CourseCard/index.jsx
+++ src/components/CourseCard/index.jsx
@@ -7,7 +7,7 @@ function CourseCard({ course }) {
     <div className="bg-white shadow-lg rounded-lg overflow-hidden transform hover:-translate-y-1 transition-transform duration-300 flex flex-col h-full">
       <div className="flex flex-col flex-grow">
         <Link to={`/cursos/${course.id}`}>
-          <img src={course.imageUrl} alt={`Imagen de ${course.title}`} className="w-full h-48 object-contain" />
+          <img src={course.imageUrl} alt={`Imagen de ${course.title}`} className="w-full h-48 object-cover" />
           <div className="p-6">
             <h3 className="text-xl font-bold text-gray-900 mb-2">{course.title}</h3>
             <p className="text-gray-600 text-base mb-4">{course.shortDescription}</p>

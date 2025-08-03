58316d2aa8f44b1cec5833eeccd71b08e52657a7
arieltecay <arieltecay@gmail.com>
2025-08-03 12:21:36 -0300

    add static information about & footer

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -8,13 +8,19 @@ const About = () => {
           <div className="md:w-1/2 mb-8 md:mb-0">
             <img src="/images/perfil.jpg" alt="Sobre Mí" className="rounded-lg shadow-lg mx-auto w-80 h-auto w-70 h-auto" />
           </div>
-          <div className="md:w-1/2 md:pl-12">
+          <div className="md:w-1/2 md:pl-12 text-justify">
             <h2 className="text-4xl font-bold text-gray-800 mb-4">Sobre Mí 🧵</h2>
             <p className="text-gray-600 mb-4 leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada. Curabitur sit amet est sed felis dignissim tincidunt.
+              ¡Hola! Soy Mica Guevara modista de alta costura, creadora de contenido y apasionada por enseñar el arte de coser.
             </p>
-            <p className="text-gray-600 leading-relaxed">
-              Praesent eu libero et diam aliquam efficitur. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            <p className="text-gray-600 mb-4 leading-relaxed">
+              Desde chica encontré en la costura una forma de expresarme y con los años transformé ese amor en una profesión. Hoy acompaño a mujeres de todas las edades a desarrollar su creatividad y emprender con sus manos.
+            </p>
+            <p className="text-gray-600 mb-4 leading-relaxed">
+              En este espacio vas a encontrar cursos prácticos, accesibles y pensados para que puedas avanzar a tu ritmo. Ya sea que estés dando tus primeros pasos o que quieras perfeccionar tus técnicas, estas en el lugar indicado.
+            </p>
+            <p className="text-gray-600 mb-4 leading-relaxed">
+              Me emociona ver como una prenda bien hecha puede cambiar cómo nos sentimos. Por eso mi propósito es ayudarte a que te sientas segura, capaz y orgullosa de lo que creas.
             </p>
           </div>
         </div>
--- src/pages/Footer/index.jsx
+++ src/pages/Footer/index.jsx
@@ -1,23 +1,21 @@
-import { FaWhatsapp, FaEnvelope, FaInstagram, FaFacebook } from 'react-icons/fa';
+import { FaWhatsapp, FaEnvelope, FaInstagram, FaTiktok } from 'react-icons/fa';
 import { whatsappNumber } from '../../utils/constants';
 
 const Footer = () => {
   return (
-    <footer id="contact" className="bg-[var(--color-green-600)] text-white py-12">
+    <footer id="contact" className="bg-[var(--color-green-600)] text-black py-12">
       <div className="container mx-auto px-6 text-center">
-        <h2 className="text-3xl font-bold mb-6">Contacto ✂️</h2>
-        <p className="mb-8 text-lg text-white">¿Tienes alguna pregunta? ¡No dudes en contactarme!</p>
+        <h2 className="text-3xl font-bold mb-6">Bienvenidos a ésta comunidad que cose sueños, puntada a puntada</h2>
+        <h4 className="text-2xl font-bold mb-6">Conectá conmigo</h4>
+        <p className="mb-8 text-lg">¿Quieres aprender más?, ¿Hacerme una pregunta o simplemente saludar? Estoy a un click de distancia</p>
         <div className="flex justify-center space-x-6 mb-8">
           <a href={whatsappNumber} target="_blank" rel="noopener noreferrer" className="text-4xl hover:text-[var(--color-green-600)] transition duration-300">
             <FaWhatsapp />
           </a>
-          <a href="mailto:hola@modista.com" className="text-4xl hover:text-[var(--color-green-600)] transition duration-300">
-            <FaEnvelope />
-          </a>
-          <a href="https://www.instagram.com/soymicaguevara?igsh=dmFicTdlZml5MHRk" className="text-4xl hover:text-[var(--color-green-600)] transition duration-300"><FaInstagram /></a>
-          <a href="#" className="text-4xl hover:text-[var(--color-green-600)] transition duration-300"><FaFacebook /></a>
+          <a href="https://www.instagram.com/soymicaguevara" className="text-4xl hover:text-[var(--color-green-600)] transition duration-300"><FaInstagram /></a>
+          <a href="#" className="text-4xl hover:text-[var(--color-green-600)] transition duration-300"><FaTiktok /></a>
         </div>
-        <p className="text-gray-500">&copy; 2025 ModistaApp. Todos los derechos reservados. 👗</p>
+        <p className="text-gray-500">&copy; 2025 Micaela Guevara Modista de alta costura. Todos los derechos reservados.</p>
       </div>
     </footer>
   );

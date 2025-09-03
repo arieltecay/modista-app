094f4e27521f039fdd12a6c1fbe3050b16d9ffa9
arieltecay <arieltecay@gmail.com>
2025-09-03 19:58:48 -0300

    delete unused \ in const

--- src/pages/Inscriptions/index.jsx
+++ src/pages/Inscriptions/index.jsx
@@ -40,7 +40,7 @@ const InscriptionsPage = () => {
   }, [courseId]);
 
   const validateEmail = (email) => {
-    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
     return re.test(String(email).toLowerCase());
   };
 

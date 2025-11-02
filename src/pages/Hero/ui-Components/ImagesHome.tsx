b60f35d17a399ce4f6e27e56a7e4bbaab1ed012e
arieltecay <arieltecay@gmail.com>
2025-11-02 13:37:44 -0300

    re order size of image in home

--- src/pages/Hero/ui-Components/ImagesHome.tsx
+++ src/pages/Hero/ui-Components/ImagesHome.tsx
@@ -1,7 +1,7 @@
 // Component that displays a single hero image occupying the full allocated space
 export const ImagesHome = () => {
   return (
-    <div className="relative overflow-hidden rounded-xl pt-[40%]">
+    <div className="relative overflow-hidden rounded-xl pt-[40%] max-sm:pt-[55%]">
       <img
         src="/images/HomeMica.jpeg"
         alt="Modista trabajando"

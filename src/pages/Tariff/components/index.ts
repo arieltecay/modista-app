f7ded4140905649827596cd45180c7149101f033
arieltecay <arieltecay@gmail.com>
2026-02-15 23:43:04 -0300

    refactor: Mueve el componente `DynamicTariffSection` y actualiza su ruta de importación.

--- src/pages/Tariff/components/index.ts
+++ src/pages/Tariff/components/index.ts
@@ -1,4 +1,4 @@
-export { default as DynamicTariffSection } from './DynamicTariffSection/DynamicTariffSection';
+export { default as DynamicTariffSection } from './DynamicTariffSection';
 export { default as TariffModista } from './TariffModista';
 export { default as TariffAltaCostura } from './TariffAltaCostura';
 export { default as TariffArreglos } from './TariffArreglos';

7b76d2fb3d017ab3d1ee5874ba6619dc2d15d839
arieltecay <arieltecay@gmail.com>
2026-02-19 21:32:39 -0300

    refactor: Mover el hook `useDebounce` a un archivo compartido para promover la reutilización.

--- src/pages/admin/inscriptions/pages/InscriptionsListPage.tsx
+++ src/pages/admin/inscriptions/pages/InscriptionsListPage.tsx
@@ -11,6 +11,7 @@ import toast from 'react-hot-toast';
 import InscriptionsListMobile from '../components/InscriptionsListMobile';
 import InscriptionsTableDesktop from '../components/InscriptionsTableDesktop';
 import Pagination from '../../shared/components/Pagination';
+import { useDebounce } from '../../shared/hooks/useDebounce';
 
 // --- Helper Components ---
 
@@ -37,19 +38,6 @@ const PendingIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-8
 const SearchIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-1/2 left-4 -translate-y-1/2 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>;
 
 
-// Hook para debounce
-const useDebounce = (value, delay) => {
-  const [debouncedValue, setDebouncedValue] = useState(value);
-  useEffect(() => {
-    const handler = setTimeout(() => {
-      setDebouncedValue(value);
-    }, delay);
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [value, delay]);
-  return debouncedValue;
-};
 
 const InscriptionsAdminPage = () => {
   const [inscriptions, setInscriptions] = useState([]);

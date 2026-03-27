9f79718deed2e1a633d080eef41a8f25116a7bd3
arieltecay <arieltecay@gmail.com>
2026-03-27 17:20:09 -0300

    delete unnecesary localhost hardcoded

--- src/services/tariff/tariffService.ts
+++ src/services/tariff/tariffService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { AvailableTariffMeta, TariffData, SearchResultItem } from './types';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const API_URL = import.meta.env.VITE_API_URL;
 
 /**
  * Obtiene el tarifario de confección desde la API, con posibilidad de filtrar.

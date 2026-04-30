2f6eeb7eed7eb995647eeda4baacb53b565ae321
arieltecay <arieltecay@gmail.com>
2026-04-30 18:04:04 -0300

    feat: add _redirects file and update PWA assets in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['dress-icon.svg', 'vite.svg'],
       manifest: {
         name: 'Modista App',
         short_name: 'Modista',
@@ -19,18 +19,15 @@ export default defineConfig({
         theme_color: '#4f46e5', // indigo-600
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: 'dress-icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
           }
         ]
       },
       workbox: {
+        cleanupOutdatedCaches: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
         runtimeCaching: [
           {

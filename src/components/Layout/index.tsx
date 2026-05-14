308d1ebdf23aa3c07baeb526f87ab6b0f6d4354c
arieltecay <arieltecay@gmail.com>
2026-05-14 11:32:37 -0300

    feat: add animation to Outlet component for smoother transitions

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import { Navbar, FloatingActionsContainer } from '@/components';
 import Footer from '../../pages/Footer';
 import { Outlet, useLocation } from 'react-router-dom';
 import FaqSection from '../FaqSection/FaqSection';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export const Layout = () => {
   const location = useLocation();
@@ -13,7 +14,17 @@ export const Layout = () => {
     <>
       <Navbar />
       <main>
-        <Outlet />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={location.pathname}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.4, ease: "easeInOut" }}
+          >
+            <Outlet />
+          </motion.div>
+        </AnimatePresence>
       </main>
       {showFaq && <FaqSection />}
       <FloatingActionsContainer />
